2116ee9ffd0f442633606d54e32fc71a628a2258
Michael Audi <maudi@blustreamcorp.com>
2020-08-20 21:20:10 -0400

    terraform cloud didn't like the variables. trying a structure of collapsing them into the resource file

--- infra/aws/resources/s3/s3.tf
+++ infra/aws/resources/s3/s3.tf
@@ -1,3 +1,7 @@
+variable "s3_bucket_name" {
+  description = "The S3 bucket name. Must be unique. Naming convention is tiki-[xyz]"
+}
+
 resource "aws_s3_bucket" "static_site" {
   bucket = var.s3_bucket_name
   acl    = "private"
--- infra/aws/resources/s3/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "s3_bucket_name" {
-  description = "The S3 bucket name. Must be unique. Naming convention is tiki-[xyz]"
-}
