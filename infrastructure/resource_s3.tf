fc18e9abdb129f7888457cff7a2902da73b410ee
Michael Audi <maudi@blustreamcorp.com>
2020-08-21 01:04:49 -0400

    added layer of security for bucket by blocking setting any new policies without first changing the block policy.

--- infrastructure/resource_s3.tf
+++ infrastructure/resource_s3.tf
@@ -1,28 +1,7 @@
 variable "aws_s3_bucket" { }
 
-data "aws_s3_bucket" "static-site" {
-  bucket = var.aws_s3_bucket
-  depends_on = [
-    aws_s3_bucket.static_site,
-  ]
-}
-
 resource "aws_s3_bucket" "static_site" {
   bucket = var.aws_s3_bucket
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-      {
-          "Sid": "PublicReadGetObject",
-          "Effect": "Allow",
-          "Principal": "*",
-          "Action": "s3:GetObject",
-          "Resource": "arn:aws:s3:::${var.aws_s3_bucket}/*"
-      }
-  ]
-}
-EOF
 
   tags = {
     Environment = var.global_tag_environment
@@ -43,17 +22,58 @@ EOF
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "static_site" {
-  bucket = data.aws_s3_bucket.static-site.bucket
+data "aws_s3_bucket" "static_site_complete" {
+  bucket = var.aws_s3_bucket
+  depends_on = [
+    aws_s3_bucket.static_site,
+  ]
+}
+
+resource "aws_s3_bucket_public_access_block" "off" {
+  bucket = data.aws_s3_bucket.static_site_complete.bucket
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "static_site" {
+  bucket = data.aws_s3_bucket.static_site_complete.bucket
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Sid": "PublicReadGetObject",
+          "Effect": "Allow",
+          "Principal": "*",
+          "Action": "s3:GetObject",
+          "Resource": "arn:aws:s3:::${var.aws_s3_bucket}/*"
+      }
+  ]
+}
+EOF
+  depends_on = [
+    aws_s3_bucket_public_access_block.off,
+  ]
+}
+
+resource "aws_s3_bucket_public_access_block" "on" {
+  bucket = data.aws_s3_bucket.static_site_complete.bucket
   block_public_acls       = true
   ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = false
+  depends_on = [
+    aws_s3_bucket_policy.static_site,
+  ]
 }
 
 resource "aws_s3_bucket_metric" "static-site" {
-  bucket = data.aws_s3_bucket.static-site.bucket
+  bucket = data.aws_s3_bucket.static_site_complete.bucket
   name   = "EntireBucket"
 }
 
 output "arn" {
-  value = aws_s3_bucket.static_site.arn
+  value = aws_s3_bucket.static_site.website_endpoint
 }
