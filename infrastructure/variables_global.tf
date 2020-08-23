a7ea3523d479666496e8b61fd6bab97f8afcb4a4
Michael Audi <maudi@blustreamcorp.com>
2020-08-22 21:31:42 -0400

    already had a test.mytiki.com site up that we didnt want to take down just yet. move it to the root which is not up yet.

--- infrastructure/variables_global.tf
+++ infrastructure/variables_global.tf
@@ -1,7 +1,7 @@
 variable "global_log_bucket" { }
 
 locals {
-  global_tag_environment    = "Test"
+  global_tag_environment    = "Production"
   global_tag_service        = "website"
-  global_website_domain     = "test.mytiki.com"
+  global_website_domain     = "mytiki.com"
 }
