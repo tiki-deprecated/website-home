8e62e47286a453841278fc5f3575e99e8164564e
Michael Audi <maudi@blustreamcorp.com>
2020-08-23 04:47:16 -0400

    s3 doesnt have https setup for the website it has to go http

--- infrastructure/resource_cloudfront.tf
+++ infrastructure/resource_cloudfront.tf
@@ -6,10 +6,12 @@ resource "aws_cloudfront_distribution" "website" {
     origin_id   = aws_s3_bucket.frontend.bucket
 
     custom_origin_config {
-      http_port = 80
-      https_port = 443
-      origin_protocol_policy = "https-only"
-      origin_ssl_protocols = "TLSv1.2"
+      http_port                = 80
+      https_port               = 443
+      origin_protocol_policy   = "http-only"
+      origin_ssl_protocols     = ["TLSv1.2"]
+      origin_keepalive_timeout = 5
+      origin_read_timeout      = 30
     }
   }
 
