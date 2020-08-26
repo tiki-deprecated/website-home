1d2732cbaa105f61901714b07cfe506ddcd2d57b
Michael Audi <maudi@blustreamcorp.com>
2020-08-26 09:41:52 -0400

    forgot to update the lambda permission.

--- infrastructure/resource_lambda.tf
+++ infrastructure/resource_lambda.tf
@@ -29,5 +29,5 @@ resource "aws_lambda_permission" "apigw" {
 
   # The "/*/*" portion grants access from any method on any resource
   # within the API Gateway REST API.
-  source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.signup.execution_arn}/*/*"
 }
