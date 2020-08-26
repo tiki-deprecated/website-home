0cc860eb7d9880e20d7fef22a59616f25ce5ce7b
Michael Audi <maudi@blustreamcorp.com>
2020-08-26 01:39:25 -0400

    testing out adding and permissions for a sample api gateway (from terraform)

--- infrastructure/resource_lambda.tf
+++ infrastructure/resource_lambda.tf
@@ -20,3 +20,14 @@ resource "aws_lambda_function" "example" {
 
   role = data.aws_iam_role.lambda_exec.arn
 }
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.example.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The "/*/*" portion grants access from any method on any resource
+  # within the API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
+}
