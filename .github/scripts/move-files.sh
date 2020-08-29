485e869430cec6bcfad92ebcdf87e57be6bb7bf2
Michael Audi <maudi@blustreamcorp.com>
2020-08-29 02:07:44 -0400

    will ./ get enable it to find the files?

--- .github/scripts/move-files.sh
+++ .github/scripts/move-files.sh
@@ -6,9 +6,9 @@
 # Used during continuous integration github workflow to move/copy
 # generated/config files into infrastructure package (infrastructure/files).
 
-cp backend/oas.json infrastructure/files/oas.json
-cp backend/api.version infrastructure/files/api.version
-cp backend/functions/functions.version infrastructure/files/functions.version
-mv backend/functions/functions.zip infrastructure/files/functions.zip
-mv frontend/dist infrastructure/files/dist
+cp ./backend/oas.json ./infrastructure/files/oas.json
+cp ./backend/api.version ./infrastructure/files/api.version
+cp ./backend/functions/functions.version ./infrastructure/files/functions.version
+mv ./backend/functions/functions.zip ./infrastructure/files/functions.zip
+mv ./frontend/dist ./infrastructure/files/dist
 
