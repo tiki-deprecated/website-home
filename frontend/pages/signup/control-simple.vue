64aa79c4d94cc79cffa358c80687b07f7421a6d7
Michael Audi <maudi@blustreamcorp.com>
2021-01-08 13:15:12 -0500

    added "how" popup

--- frontend/pages/signup/control-simple.vue
+++ frontend/pages/signup/control-simple.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="controlSimplePage">
-    <div class="controlSimplePageWrapper">
-      <header-simple-cmp
-        class="headerSimpleCmp"
-        heading="Take back control of your data, with **a single app**."
-      />
-      <form-simple-cmp class="formSimpleCmp" />
-      <notified-cmp class="notifiedCmp" />
+    <div class="controlSimplePageStack">
+      <div class="controlSimplePageWrapper">
+        <header-simple-cmp
+          class="headerSimpleCmp"
+          heading="Take back control of your data, with **a single app**."
+        />
+        <form-simple-cmp class="formSimpleCmp" />
+        <notified-cmp class="notifiedCmp" />
+      </div>
+      <footer-cmp class="footerCmp" />
     </div>
-    <footer-cmp class="footerCmp" />
+    <popup-privacy-cmp class="controlSimplePopupDataCmp" />
   </div>
 </template>
 
@@ -25,8 +28,25 @@ export default {
 </script>
 
 <style scoped lang="sass">
+.controlSimplePage
+  position: relative
+
+.controlSimplePageStack
+  position: absolute
+  top: 0
+  left: 0
+  z-index: 1
+  width: 100%
+
 .controlSimplePageWrapper
+  position: relative
   display: flex
   flex-direction: column
   min-height: 100vh
+
+.controlSimplePopupDataCmp
+  position: fixed
+  z-index: 9
+  width: 100%
+  height: 100%
 </style>
