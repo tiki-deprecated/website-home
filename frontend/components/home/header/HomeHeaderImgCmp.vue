9286430ab0527c0b7c47593257e9ea33181f2713
Mike Audi <mike@mytiki.com>
2021-05-13 23:15:43 -0400

    update pineapple to match app
    fixes #9

--- frontend/components/home/header/HomeHeaderImgCmp.vue
+++ frontend/components/home/header/HomeHeaderImgCmp.vue
@@ -1,57 +1,41 @@
 <template>
   <div>
-    <div class="imgLgHide">
-      <utils-svg-cmp name="home/header/bkg-img-lg" class="bkgImgLg" />
-    </div>
-    <div class="imgHide">
-      <utils-svg-cmp name="home/header/bkg-img" class="bkImg" />
+    <div class="imgWrapper">
+      <img
+        sizes="(max-width: 768px) 100vw, 768px"
+        srcset="
+          ~/assets/images/png/home/header/pineapple_w_200.png   200w,
+          ~/assets/images/png/home/header/pineapple_w_1030.png 1030w,
+          ~/assets/images/png/home/header/pineapple_w_1400.png 1400w
+        "
+        src="~/assets/images/png/home/header/pineapple_w_1400.png"
+        alt=""
+        class="img"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import UtilsSvgCmp from '../../utils/UtilsSvgCmp'
-
 export default {
   name: 'HomeHeaderImgCmp',
-  components: { UtilsSvgCmp },
 }
 </script>
 
 <style scoped lang="sass">
 @import "assets/styles/mixins"
 
-.bkgImgLg, .bkImg
+.imgWrapper
   position: absolute
   z-index: 1
-
-::v-deep .bkgImgLg.svg > .layer1, ::v-deep .bkgImgLg.svg > .lens, ::v-deep .bkImg.svg > .layer1, ::v-deep .bkImg.svg > .lens
-  fill: $orange
-
-::v-deep .bkgImgLg.svg > .layer2, ::v-deep .bkImg.svg > .layer2
-  fill: $yellow-xlight
-
-::v-deep .bkgImgLg.svg > .frames, ::v-deep .bkImg.svg > .frames
-  fill: $black-dark
-
-::v-deep .bkgImgLg.svg > .pineapple, ::v-deep .bkImg.svg > .pineapple
-  fill: $black-light
+  right: 0
+  bottom: 0
 
 @include for-phone
-  .imgLgHide
-    display: none
-
-  ::v-deep .bkImg.svg
+  .img
     height: 40vw
-    right: 0
-    bottom: 22%
 
 @include for-tablet
-  .imgHide
-    display: none
-
-  ::v-deep .bkgImgLg.svg
-    height: 17vw
-    right: 0
-    top: 17%
+  .img
+    height: 25vw
 </style>
