2a3d76549dd1f4722ef287e70074da5f85f5733e
Mike Audi <mike@mytiki.com>
2021-06-09 01:04:19 -0400

    fixes for post lambda

--- backend/functions/user/post.js
+++ backend/functions/user/post.js
@@ -4,12 +4,11 @@ const helpers = require("../../utils/helpers.js");
 const sendgrid = require("../../utils/sendgrid");
 
 exports.handler = function (event, context, callback) {
-  if (event.body == null)
-    callback(null, { statusCode: "400", headers: helpers.corsHeaders });
+  if (event.body == null) callback(null, helpers.badRequest());
 
   const body = JSON.parse(event.body);
   if (body.email == null || !helpers.isEmail(body.email))
-    callback(null, { statusCode: "400", headers: helpers.corsHeaders });
+    callback(null, helpers.badRequest());
 
   body.email = helpers.sanitizeString(body.email);
   body.referrer = helpers.sanitizeString(body.referrer);
@@ -25,7 +24,7 @@ exports.handler = function (event, context, callback) {
     },
     function (err, data) {
       if (err) console.log(err, err.stack);
-      if (data.statusCode === 200) {
+      if (data && helpers.is2xx(data.code)) {
         callback(null, {
           statusCode: "200",
           headers: helpers.corsHeaders,
@@ -36,11 +35,7 @@ exports.handler = function (event, context, callback) {
             confirmed: body.confirmed,
           }),
         });
-      } else
-        callback(null, {
-          statusCode: "422",
-          headers: helpers.corsHeaders,
-        });
+      } else callback(null, helpers.badRequest());
     }
   );
 };
--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -11,6 +11,9 @@ module.exports = {
   isPhone(s) {
     return /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/.test(s);
   },
+  is2xx(code) {
+    return code == null ? false : Math.trunc(code / 100) === 2;
+  },
   obfuscateEmail(email) {
     return email.replace(email.substring(0, email.indexOf("@")), "*****");
   },
@@ -19,6 +22,12 @@ module.exports = {
       "Access-Control-Allow-Headers": "Content-Type",
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "OPTIONS,POST",
+    };
+  },
+  badRequest() {
+    return {
+      statusCode: "400",
+      headers: this.corsHeaders(),
     };
   },
 };
