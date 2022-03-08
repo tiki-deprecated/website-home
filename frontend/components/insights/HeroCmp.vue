8ba434778145dcb17fb2834c7377365c45d8ff07
Mike Audi <mike@mytiki.com>
2022-03-08 13:59:01 -0600

    basic folder/page structure for insights landing page

---
+++ frontend/components/insights/HeroCmp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hero">Hello World</div>
+</template>
+
+<script>
+export default {
+  name: 'HeroCmp',
+}
+</script>
+
+<style scoped lang="sass">
+@import "assets/styles/mixins"
+
+@include for-phone
+
+@include for-tablet
+</style>
---
+++ frontend/pages/insights.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page">
+    Hello World
+    <utils-footer-cmp />
+  </div>
+</template>
+
+<script>
+import UtilsFooterCmp from '../components/utils/UtilsFooterCmp'
+
+export default {
+  name: 'Insights',
+  components: {
+    UtilsFooterCmp,
+  },
+  mounted() {
+    this.$plausible.trackPageview()
+  },
+}
+</script>
+
+<style scoped lang="sass">
+@import "assets/styles/mixins"
+
+.page
+  width: 100%
+  position: relative
+</style>
