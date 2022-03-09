ff80302cfabb556e9091453c572e31f7d38c91ce
NickkTMD <nickk@havocmc.net>
2022-03-09 16:46:49 -0600

    Added apply comp

--- frontend/pages/insights.vue
+++ frontend/pages/insights.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="page">
     <hero-cmp />
+    <apply-for-access />
     <features-cmp />
     <company-info-cmp />
     <utils-footer-cmp />
@@ -12,11 +13,13 @@ import FeaturesCmp from '../components/insights/FeaturesCmp'
 import UtilsFooterCmp from '../components/utils/UtilsFooterCmp'
 import HeroCmp from '../components/insights/HeroCmp'
 import CompanyInfoCmp from '../components/insights/CompanyInfoCmp'
+import ApplyForAccess from '../components/insights/ApplyForAccess'
 
 export default {
   name: 'InsightsPage',
   components: {
     HeroCmp,
+    ApplyForAccess,
     FeaturesCmp,
     CompanyInfoCmp,
     UtilsFooterCmp,
