d1b98cf58980b73016e2bc08c299d9500264357e
Miro Benício <96149507+MiroBenicio@users.noreply.github.com>
2023-12-16 01:08:16 -0300

    Feature/create a auth screen (#179)
    ### Changes
    
    - Adds an auth screen modal that prompts the user for their auth token
    
    ---------
    
    Co-authored-by: Tim O'Guin <tim@mytiki.com>

--- subscription-ui/src/App.vue
+++ subscription-ui/src/App.vue
@@ -1,60 +1,22 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
-import queryEditor from './components/queryEditor.vue'
-import queryInfo from './components/queryInfo.vue'
-import infoButtons from './components/infoButtons.vue'
-import tableTaxonomy from './components/tableTaxonomy.vue'
-import cleanroomSelect from './components/cleanroomSelect.vue'
-import type { QueryInfo } from './interfaces/QueryInfo'
+import { onMounted, ref } from 'vue'
+import SubscriptionUi from './components/SubscriptionUi.vue'
+import AuthSheet from './components/AuthSheet.vue'
 
-const selectedTable = ref<string>()
-const infoState = ref<string>()
-const info = ref<QueryInfo>()
+const token = ref<string>()
 
-const updateInfo = (infoJson: QueryInfo) => {
-  info.value = infoJson
-  infoState.value = 'cost'
+const handleToken = (submittedToken: string) => {
+  token.value = submittedToken
+  sessionStorage.setItem('authToken', token.value)
 }
-
-const panelWidth = ref<number>(350)
-
-const remWidth = computed(() => {
-  return `${panelWidth.value / 16}rem`
+onMounted(() => {
+  token.value = sessionStorage.getItem('authToken') ?? ''
 })
-
-const resize = (e: MouseEvent) => {
-  if(!isResized.value) return
-
-  panelWidth.value = panelWidth.value + e.movementX / 2
-}
-
-const isResized = ref<boolean>(false);
-
-const datafield = ref<string>();
 </script>
 
-<template >
-  <div class="w-full h-screen grid" @mousemove="resize" @mouseup="isResized = false">
-    <div class="flex mx-10">
-      <div class="mt-5" :style="{ width: remWidth }" v-if="panelWidth > 256">
-        <div class="flex-col">
-          <cleanroom-select @update="(newValue) => (selectedTable = newValue)" @close="panelWidth = 299"/>
-          <table-taxonomy @insert="(value) => datafield = value"/>
-        </div>
-      </div>
-      <div class="w-1 bg-light-gray cursor-col-resize shadow-md border-left border-solid border-black" @mousedown="isResized = true" ></div>
-      <div class="grow px-8 mt-5">
-        <query-editor :table="selectedTable" @update="updateInfo" :datafield="datafield"/>
-        <info-buttons
-          @cost="infoState = 'cost'"
-          @sample="infoState = 'sample'"
-          @stats="infoState = 'stats'"
-          :state-prop="infoState"
-        />
-        <query-info :state="infoState" :info="info" />
-      </div>
-    </div>
-  </div>
+<template>
+  <auth-sheet v-if="!token" @submit="handleToken" />
+  <subscription-ui />
 </template>
 
 <style scoped></style>
