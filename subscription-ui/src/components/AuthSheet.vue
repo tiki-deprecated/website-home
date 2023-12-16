e0dd5ac53674964ae4f2b81fa73a6162d5be44bf
Miro Benicio <mirodantas27@gmail.com>
2023-12-16 00:06:27 -0300

    Feature: Auth Screen

---
+++ subscription-ui/src/components/AuthSheet.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { ref, defineEmits } from 'vue';
+
+const emit = defineEmits(['submit'])
+
+const submit = () => {
+    if(token.value !== undefined && token.value !== ''){
+        emit('submit', token.value)
+        return
+    }
+    error.value = 'The token field should not be empty'
+}
+
+const token = ref<string>()
+
+const error = ref<string>()
+
+</script>
+
+<template>
+  <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+    <div class="fixed inset-0 bg-black bg-opacity-70 transition-opacity"></div>
+
+    <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+      <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <div
+          class="bg-white flex-col gap-6 flex justify-center items-center relative overflow-hidden rounded-lg text-left shadow-xl transition-all px-12 w-[80vw] h-[80vh]"
+        >
+          <img src="../assets/images/pineapple_rounded.svg" alt="" class="w-32" />
+          <h1 class="text-3xl">Hey! Thanks for choose TIKI</h1>
+          <p>
+            Before you can generate an estimate, we need your auth token to make it happen.
+            <a href="https://mytiki.com/reference/intro/authentication" target="_blank" class="text-green">Click here to get it.</a>
+          </p>
+          <input
+            type="text"
+            class="border border-solid border-dark-gray/20 rounded-lg px-4 py-1"
+            placeholder="Paste your token here..."
+            v-model="token"
+            @keydown="error = ''"
+          />
+          <button class="border py-2 bg-green rounded-xl w-60 text-white hover:bg-green/70" @click="submit">
+            Continue
+          </button>
+          <p v-if="error" class="text-red text-sm font-normal">{{ error }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ subscription-ui/src/components/SubscriptionUi.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import queryEditor from './queryEditor.vue'
+import queryInfo from './queryInfo.vue'
+import infoButtons from './infoButtons.vue'
+import tableTaxonomy from './tableTaxonomy.vue'
+import cleanroomSelect from './cleanroomSelect.vue'
+import type { QueryInfo } from '../interfaces/QueryInfo'
+
+const selectedTable = ref<string>()
+const infoState = ref<string>()
+const info = ref<QueryInfo>()
+
+const updateInfo = (infoJson: QueryInfo) => {
+  info.value = infoJson
+  infoState.value = 'cost'
+}
+
+const panelWidth = ref<number>(350)
+
+const remWidth = computed(() => {
+  return `${panelWidth.value / 16}rem`
+})
+
+const resize = (e: MouseEvent) => {
+  if (!isResized.value) return
+
+  panelWidth.value = panelWidth.value + e.movementX / 2
+}
+
+const isResized = ref<boolean>(false)
+
+const datafield = ref<string>()
+</script>
+
+<template>
+  <div class="w-full h-screen grid" @mousemove="resize" @mouseup="isResized = false">
+    <div class="flex mx-10">
+      <div class="mt-5" :style="{ width: remWidth }" v-if="panelWidth > 256">
+        <div class="flex-col">
+          <cleanroom-select
+            @update="(newValue: string) => (selectedTable = newValue)"
+            @close="panelWidth = 299"
+          />
+          <table-taxonomy @insert="(value:string) => (datafield = value)" />
+        </div>
+      </div>
+      <div
+        class="w-1 bg-light-gray cursor-col-resize shadow-md border-left border-solid border-black"
+        @mousedown="isResized = true"
+      ></div>
+      <div class="grow px-8 mt-5">
+        <query-editor :table="selectedTable" @update="updateInfo" :datafield="datafield" />
+        <info-buttons
+          @cost="infoState = 'cost'"
+          @sample="infoState = 'sample'"
+          @stats="infoState = 'stats'"
+          :state-prop="infoState"
+        />
+        <query-info :state="infoState" :info="info" />
+      </div>
+    </div>
+  </div>
+</template>
