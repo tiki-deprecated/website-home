562aa9a6a80ce4a741ebf44746507e462606b11d
Tim O'Guin <timoguin@gmail.com>
2023-12-18 16:55:46 -0600

    initial integration with Highlight for observability, fix favicon and page title

--- subscription-ui/src/main.ts
+++ subscription-ui/src/main.ts
@@ -1,6 +1,18 @@
 import "./assets/style.css"
 
+import { H } from 'highlight.run';
+
 import { createApp } from 'vue'
 import App from './App.vue'
+
+// TODO: These are placeholder values for initial testing. Feed them in from env at build-time.
+H.init('0dqvlnqg', {
+    environment: 'localhost',
+    version: 'commit:4b99be80865532e6c88297841c47acc6647ea18a',
+    networkRecording: {
+        enabled: true,
+        recordHeadersAndBody: true,
+    },
+});
 
 createApp(App).mount('#app')
