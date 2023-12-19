1506b7460e295f07aa90ea17165d43ca74bdc29d
Miro Benício <96149507+MiroBenicio@users.noreply.github.com>
2023-12-19 16:00:20 -0300

    feature: validate auth token (#189)
    ### Changes
    
    - Auth token is now validated on the initial auth screen
    
    Closes #187

--- subscription-ui/src/components/AuthSheet.vue
+++ subscription-ui/src/components/AuthSheet.vue
@@ -1,20 +1,30 @@
 <script setup lang="ts">
-import { ref, defineEmits } from 'vue';
+import { ref, defineEmits } from 'vue'
+import { Subscription } from '@/subscription'
+import type { ProfileInfo } from '@/interfaces/ProfileInfo'
+
+const subscription = new Subscription()
 
 const emit = defineEmits(['submit'])
 
-const submit = () => {
-    if(token.value !== undefined && token.value !== ''){
-        emit('submit', token.value)
-        return
+const submit = async () => {
+  if (token.value !== undefined && token.value !== '') {
+    const response: ProfileInfo = await subscription.validate(token.value)
+    if (!response.orgId && !response.userId) {
+      error.value = 'Invalid auth token'
+      return
     }
-    error.value = 'The token field should not be empty'
+    sessionStorage.setItem('userId', response.userId)
+    sessionStorage.setItem('orgId', response.orgId)
+    emit('submit', token.value)
+    return
+  }
+  error.value = 'The token field should not be empty'
 }
 
 const token = ref<string>()
 
 const error = ref<string>()
-
 </script>
 
 <template>
@@ -24,13 +34,18 @@ const error = ref<string>()
     <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
       <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
         <div
-          class="bg-white flex-col gap-6 flex justify-center items-center relative overflow-hidden rounded-lg text-left shadow-xl transition-all px-12 w-[80vw] h-[80vh]"
+          class="bg-white flex-col gap-3 flex justify-center items-center relative overflow-hidden rounded-lg text-left shadow-xl transition-all px-12 w-[80vw] h-[80vh]"
         >
           <img src="../assets/images/pineapple_rounded.svg" alt="" class="w-32" />
           <h1 class="text-3xl">Hey! Thanks for choosing TIKI!</h1>
           <p>
             Before you can generate an estimate, we need your auth token to make it happen.
-            <a href="https://mytiki.com/reference/intro/authentication" target="_blank" class="text-green">Click here to get it.</a>
+            <a
+              href="https://mytiki.com/reference/intro/authentication"
+              target="_blank"
+              class="text-green"
+              >Click here to get it.</a
+            >
           </p>
           <input
             type="password"
@@ -39,10 +54,15 @@ const error = ref<string>()
             v-model="token"
             @keydown="error = ''"
           />
-          <button class="border py-2 bg-green rounded-xl w-60 text-white hover:bg-green/70" @click="submit">
+          <p v-show="error" class="text-red text-sm font-normal">{{ error }}</p>
+          <button
+            class="border py-2 bg-green rounded-xl w-60 text-white hover:bg-green/70 mt-"
+            @click="submit"
+            :disabled="!token"
+            :class="!token ? 'bg-green/50' : ''"
+          >
             Continue
           </button>
-          <p v-if="error" class="text-red text-sm font-normal">{{ error }}</p>
         </div>
       </div>
     </div>
