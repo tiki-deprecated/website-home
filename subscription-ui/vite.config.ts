1b2c8d113d04687e786b59849e9f6510dc89a90b
Tim O'Guin <timoguin@gmail.com>
2024-01-04 15:32:05 -0600

    experimenting with vite-plugin-favicon

--- subscription-ui/vite.config.ts
+++ subscription-ui/vite.config.ts
@@ -3,12 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
+import ViteFaviconsPlugin from 'vite-plugin-favicon'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    svgLoader()
+    svgLoader(),
+    ViteFaviconsPlugin({
+      logo: './src/assets/images/tiki-logo.png'
+    })
   ],
   css: {
     devSourcemap: true
