8444f04f7d09d142d985deb0b0bce910a8447a15
Miro Benicio <mirodantas27@gmail.com>
2024-01-17 11:11:14 -0300

    wip: estimate builder screen

---
+++ subscription-ui/src/components/InputComponent.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import cleanroomSelect from './cleanroomSelect.vue'
+
+defineEmits(['update'])
+
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  placeholder: {
+    type: String,
+    required: false
+  }
+})
+
+const componentHandler = (type: string) => {
+  switch (type) {
+    case 'cleanroomSelect':
+      return cleanroomSelect
+    default:
+      return type
+  }
+}
+</script>
+
+<template>
+  <h1 class="mt-4 text-tiki-gray font-semibold">{{ title }}</h1>
+  <label for="" class="text-tiki-gray font-normal mt-1 block">
+    {{ description }}
+  </label>
+  <component
+    :is="componentHandler(type)"
+    class="block border border-tiki-black/10 border-solid rounded-md flex gap-2 p-2.5 mt-4 w-full placeholder:text-tiki-gray/70"
+    :placeholder="placeholder"
+    @change="(value:string)=> $emit('update', value)"
+  ></component>
+</template>
--- subscription-ui/src/components/SubscriptionUi.vue
+++ subscription-ui/src/components/SubscriptionUi.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 import queryEditor from './queryEditor.vue'
 import queryInfo from './queryInfo.vue'
 import tableTaxonomy from './tableTaxonomy.vue'
 import cleanroomSelect from './cleanroomSelect.vue'
 import type { QueryInfo } from '../interfaces/QueryInfo'
 import LoadingScreen from './LoadingScreen.vue'
+import InputComponent from './InputComponent.vue'
+import HeaderTitle from './HeaderTitle.vue'
 
 const cleanroomId = ref<string>()
 const info = ref<QueryInfo>()
@@ -15,16 +17,6 @@ const updateInfo = (infoJson: QueryInfo) => {
 }
 
 const panelWidth = ref<number>(350)
-
-const remWidth = computed(() => {
-  return `${panelWidth.value / 16}rem`
-})
-
-const resize = (e: MouseEvent) => {
-  if (!isResized.value) return
-
-  panelWidth.value = panelWidth.value + e.movementX / 2
-}
 
 const isResized = ref<boolean>(false)
 
@@ -36,43 +28,20 @@ const isLoading = ref<boolean>(false)
 </script>
 
 <template>
-  <div class="w-full h-screen grid absolute" @mousemove="resize" @mouseup="isResized = false">
-    <div class="flex mx-10">
-      <div class="mt-5" :style="{ width: remWidth }" v-if="panelWidth > 250">
-        <div class="flex-col">
-          <cleanroom-select
-            @update="(newValue: string) => (cleanroomId = newValue)"
-            @close="panelWidth = 0"
-          />
-          <table-taxonomy
-            @insert="(value: string) => (datafield = value)"
-            @update="(table: string) => (tableName = table)"
-          />
-        </div>
-      </div>
-      <div class="w-10 mt-4 mr-2" v-else>
-        <button
-          class="flex border border-light-gray border-solid rounded-lg shadow-md p-1"
-          @click="panelWidth = 350"
-        >
-          <img src="../assets/images/collapse-right.svg" class="w-6" alt="" />
-        </button>
-      </div>
-      <div
-        class="w-1 bg-light-gray cursor-col-resize shadow-md border-left border-solid border-black"
-        @mousedown="isResized = true"
-      ></div>
-      <div class="grow px-8 mt-5">
-        <query-editor
-          @update="updateInfo"
-          :datafield="datafield"
-          :cleanroomId="cleanroomId!"
-          :table="tableName"
-          @loading="(value: boolean) => (isLoading = value)"
-        />
-        <query-info :info="info" v-if="info" @close="info = undefined" />
-      </div>
+    <div class="w-3/5 m-5 px-10 py-4 border-r border-solid border-tiki-black/10">
+      <header-title />
+      <input-component 
+      :title="'CLEANROOM'"
+      :description="'Lorem ipsun dolor sit amet, consectur adipisciing elit.'"
+      :type="'cleanroomSelect'"
+      @update="console.log('teste1')"
+      />
+      <input-component 
+      :title="'TABLE NAME'"
+      :description="'Lorem ipsun dolor sit amet, consectur adipisciing elit.'"
+      :type="'input'"
+      :placeholder="'my_first_table'"
+      @update="console.log('teste2')"
+      />
     </div>
-  </div>
-  <loading-screen v-if="isLoading" />
 </template>
