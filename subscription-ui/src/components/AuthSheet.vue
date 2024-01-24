1d2d0c7b80325d1257f2167044a275284e7f71e2
Miro Benicio <mirodantas27@gmail.com>
2024-01-24 11:01:26 -0300

    fix: error alert in auth and subscribe screen

--- subscription-ui/src/components/AuthSheet.vue
+++ subscription-ui/src/components/AuthSheet.vue
@@ -5,6 +5,8 @@ import type { ProfileInfo } from '@/interfaces/ProfileInfo'
 import HeaderTitle from './HeaderTitle.vue'
 import TextButton from './TextButton.vue'
 import { ButtonState } from '@/interfaces/ButtonState'
+import AlertComponent from './AlertComponent.vue'
+import { BuilderState } from '@/interfaces/BuilderState'
 
 const subscription = new Subscription()
 
@@ -33,6 +35,14 @@ const error = ref<string>()
 <template>
   <div class="w-3/5 m-5 px-10 py-4 border-r border-solid border-tiki-black/10">
     <header-title />
+    <alert-component
+      :type="
+        BuilderState.ERROR
+      "
+      :text="'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'"
+      v-if="error"
+      @cancel="error = undefined"
+    />
     <h1 class="mt-4 text-tiki-gray font-semibold">API KEY</h1>
     <label for="" class="text-tiki-gray font-normal mt-1 block">
       Copy and paste your API Key from
