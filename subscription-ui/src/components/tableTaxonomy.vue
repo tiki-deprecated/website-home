feaf69f1cccf3e43672e1155027cce6e72e68b64
Miro Benicio <mirodantas27@gmail.com>
2024-01-24 12:18:33 -0300

    clean defineEmit import

--- subscription-ui/src/components/tableTaxonomy.vue
+++ subscription-ui/src/components/tableTaxonomy.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { demographics, transactions, receipts } from '../interfaces/TableTaxonomies'
-import { defineEmits } from 'vue'
 
 const tables = ['demographics', 'receipts', 'transactions']
 
