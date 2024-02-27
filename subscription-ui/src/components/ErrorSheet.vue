c23aba41be9d31e40becf00d6f889c697db821c7
Miro Benicio <mirodantas27@gmail.com>
2024-02-27 17:19:37 -0300

    fix: Error Sheet

--- subscription-ui/src/components/ErrorSheet.vue
+++ subscription-ui/src/components/ErrorSheet.vue
@@ -1,20 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ButtonState } from '@/interfaces/ButtonState';
+import HeaderTitle from './HeaderTitle.vue';
+import TextButton from './TextButton.vue';
+
+
+const goToLogin = () => {
+  window.location.href = 'https://account.mytiki.com/pages/login'
+}
+</script>
 
 <template>
-  <div class="flex items-start justify-center w-full">
-    <div class="flex flex-col justify-center items-center">
-      <img src="../assets/images/tiki-pineapple-block.png" alt="" class="w-40" />
-      <label for="" class="text-tiki-gray font-normal mt-1 block text-lg">
-        Hey, something went wrong with your authentication, try to redirect the page or
-        <a
-          class="text-green underline"
-          href="https://account.mytiki.com/pages/login"
-          target="_blank"
-          >login</a
-        >
-        to start.
-      </label>
-    </div>
+  <div class="w-3/5 pr-10 border-r border-solid border-tiki-black/10">
+    <header-title />
+    <h1 class="mt-8 text-tiki-gray font-normal text-md">Please Log In to access this page</h1>
+    <text-button
+      :state="ButtonState.ACTIVE"
+      :text="'Log In'"
+      @submit="goToLogin"
+    />
+  </div>
+  <div class="w-2/5 flex justify-center items-center">
+    <img src="../assets/images/tiki-pineapple-block.png" alt="" class="w-40" />
   </div>
 </template>
 
